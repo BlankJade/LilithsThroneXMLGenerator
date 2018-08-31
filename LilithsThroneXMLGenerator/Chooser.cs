922a2e97e001f4cc65305f9bdfac3628216eae63
BlankJade <BlankJade@users.noreply.github.com>
2018-08-31 20:40:27 +0200

    added check for DataSources folder

--- LilithsThroneXMLGenerator/Chooser.cs
+++ LilithsThroneXMLGenerator/Chooser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,33 +17,40 @@ namespace LilithsThroneXMLGenerator
 		{
 			InitializeComponent();
 		}
-
+		public static string path = Directory.GetCurrentDirectory();
 		private void button1_Click(object sender, EventArgs e)
 		{
-			if(modChooser.Text == "Clothing")
+			if (Directory.Exists(path + "/DataSources/Data.xml") == true)
+			{
+				if (modChooser.Text == "Clothing")
 			{
 				Hide();
 				ClothingForm clothingForm = new ClothingForm();
 				clothingForm.ShowDialog();
 				Close();
 			}
-			if(modChooser.Text == "Weapon")
+				if (modChooser.Text == "Weapon")
 			{
 				Hide();
 				WeaponForm weaponForm = new WeaponForm();
 				weaponForm.ShowDialog();
 				Close();
 			}
-			if(modChooser.Text == "Tattoo")
+				if (modChooser.Text == "Tattoo")
 			{
 				Hide();
 				TattooForm tattooForm = new TattooForm();
 				tattooForm.ShowDialog();
 				Close();
 			}
-			else
+				else
 			{
 				MessageBox.Show("You need to choose which type of mod you want to create.");
+			}
+			}
+			else
+			{
+				MessageBox.Show("You are missing the DataSources directory. It needs to be in the same folder as this .EXE.");
 			}
 		}
 	}
